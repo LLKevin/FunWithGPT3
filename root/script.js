232a4b2a1887a4dcd5f7ec102e397523e032092e
Kevin <kevinleo.lucas@gmail.com>
2022-05-17 22:44:09 -0400

    complete JavaScript Logic and adjust styling for the responses

--- root/script.js
+++ root/script.js
@@ -1,9 +1,69 @@
-prompt = document.getElementById('txtprompt');
+
+document.getElementById('frm_prompt').addEventListener("submit",   function(event){
+    prompt = document.getElementById('txtprompt');
+    if(prompt.value != ""){
+        handleSubmit(prompt);
+    }
+    event.preventDefault();
+})
 
 
-
-function generatePrompt(prompt){
-
+async function handleSubmit(prompt){
+    let response  = await callApi(prompt.value)
+    createResponse(prompt.value, response);
 }
 
+
+
+function createResponse(prompt, response){
+
+
+   let response_div = document.createElement("div");
+   let response_container = document.getElementById("response_container");
+
+   let prompt_para = document.createElement('p');
+   prompt_para.innerHTML = prompt;
+   
+   let prompt_title = document.createElement('h3');
+   prompt_title.innerHTML = "Prompt:";
+
+   let response_title = document.createElement('h3');
+   response_title.innerHTML = "Response:";
+
+   let response_para = document.createElement('p');
+   response_para.innerHTML = response;
+
+   response_div.append(prompt_title)
+   response_div.append(prompt_para)
+   response_div.append(response_title)
+   response_div.append(response_para)
+   response_container.append(response_div);
+   
+}
+
+ async function callApi(prompt){
+    let api_response = "";
+    const data = {
+        prompt: prompt,
+        temperature: 0.5,
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+       };
+        
+       const response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+        },
+        body: JSON.stringify(data),
+       })
+       .then(res  => res.json())
+       .then(data => api_response = data.choices[0].text
+       );
+
+       return api_response
+}
 
